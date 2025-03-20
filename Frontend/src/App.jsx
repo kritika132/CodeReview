2941bbadf6409b60905b5726e1b3f3978a9b3b9f
kritika <139483333+kritika132@users.noreply.github.com>
2025-03-20 11:10:43 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   })
 
   async function reviewCode() {
-      const response = await axios.post('http://localhost:3000/ai/get-review', {code})
+      const response = await axios.post('https://codereview-4.onrender.com/ai/get-review', {code})
 
       // console.log(response.data);
       setReview(response.data);
